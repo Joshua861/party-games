8210f19b0cce24677163ac7fb856ac8149411be3
Joshua Lopes <joshrl@proton.me>
2023-11-02 15:45:14 +0000

    fixed x-oveflow on mobile

--- src/routes/wrapper.svelte
+++ src/routes/wrapper.svelte
@@ -2,12 +2,14 @@
 	import Navbar from './navbar.svelte';
 </script>
 
-<Navbar class="w-[999em]" />
+<div class="overflow-x-hidden">
+	<Navbar class="w-[999em]" />
 
-<div
-	class="px-5 my-5 mx-auto prose prose-img:rounded-xl prose-a:decoration-2 prose-a:underline-offset-4 prose-a:font-bold lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert"
->
-	<slot />
+	<div
+		class="px-5 my-5 mx-auto prose prose-img:rounded-xl prose-a:decoration-2 prose-a:underline-offset-4 prose-a:font-bold lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert"
+	>
+		<slot />
 
-	<br /><br />
+		<br /><br />
+	</div>
 </div>
