b42b55bb4a1b85fc9424ab0cad9351eeead8f5ea
Joshua Lopes <joshrl@proton.me>
2023-11-03 09:16:10 +0000

    Working on trivia

--- src/routes/trivia/+page.svelte
+++ src/routes/trivia/+page.svelte
@@ -9,75 +9,29 @@
 	import { ChevronRight, EyeOff, Eye } from 'lucide-svelte';
 	import { fade } from 'svelte/transition';
 
-	let q = '';
-	let a = '';
-	let show = false;
-
-	Array.prototype.random = function () {
-		return this[Math.floor(Math.random() * this.length)];
+	const getQuestion = async () => {
+		let response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
+		console.log('Fetched response!');
+		let obj = JSON.parse(response);
+		let catagory = obj.results.category;
+		let diff = obj.results.diffuculty;
+		let q = obj.results.question;
+		let ca = obj.results.correct_answer;
+		let ia = obj.results.incorrect_answers;
 	};
 
 	onMount(() => {
-		refresh();
+		getQuestion();
 	});
-
-	let rows = [];
-
-	function refresh() {
-		console.log('Loading question...');
-		let row = rows.random();
-		q = row['QUESTIONS'];
-		a = row['ANSWERS'];
-		console.log('Q: ' + q + '\nA: ' + a);
-		show = false;
-	}
-
-	function toggleAnswer() {
-		show = !show;
-	}
 </script>
-
-TODO: https://opentdb.com/api.php?amount=1&type=multiple
 
 <h1>Trivia</h1>
 
-<blockquote class="text-lg">
-	Q: {q}
-	<br /><br />
-	<div on:click={toggleAnswer}>
-		{#if show}
-			<span in:fade={{ duration: 200 }} class="transition-all">A: {a}</span>
-		{/if}
-		{#if show == false}
-			<div in:fade={{ duration: 200 }}>
-				<Skeleton class="w-[15em] h-[1.75rem]" />
-			</div>
-		{/if}
-	</div>
-</blockquote>
-<Separator class="my-4" />
-<div class="flex items-center space-x-2 h-12">
-	<div class="flex-1 p-0 m-0" on:keypress={toggleAnswer} on:click={toggleAnswer}>
-		<Button class="w-full">
-			{#if !show}
-				<div in:fade={{ duration: 200 }}>
-					<Eye class="inline mr-3" />
-					Show answer
-				</div>
-			{/if}
-			{#if show}
-				<div in:fade={{ duration: 200 }}>
-					<EyeOff class="inline mr-3" />
-					Hide answer
-				</div>
-			{/if}
-		</Button>
-	</div>
-	<Separator orientation="vertical" class="!mx-5 !my-0" />
-	<div class="flex-1" on:click={refresh} on:keypress={refresh}>
-		<Button class="w-full">
-			<ChevronRight class="inline mr-3" />
-			Next
-		</Button>
-	</div>
-</div>
+<p class="lead">
+	{q}
+</p>
+
+<p class="text-sm">
+	Catagory: {catagory}
+	Difficulty: {diff}
+</p>
