8294973ebbcfc8a108658cd8084c2711a8559004
Joshua Lopes <joshrl@proton.me>
2023-12-04 18:15:57 +0000

    verifiied for google

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,7 @@
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 	<link rel="manifest" href="/site.webmanifest" />
 	<meta name="theme-color" content="#0A0A0A" />
+	<meta name="google-site-verification" content="DyQuk21ziFyRNfxYbKS8jO-laFmZ-g9GZa0qYovhGbc" />
 </svelte:head>
 
 <ModeWatcher />
